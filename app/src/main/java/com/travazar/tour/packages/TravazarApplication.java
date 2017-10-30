91eaddb5fd221dd5025721993f65b8cba9ecf59e
Joseph Mangmang <joseph.deplacer@gmail.com>
2017-10-30 17:16:03 +0800

    add leakcanary

--- app/src/main/java/com/travazar/tour/packages/TravazarApplication.java
+++ app/src/main/java/com/travazar/tour/packages/TravazarApplication.java
@@ -3,6 +3,7 @@ package com.travazar.tour.packages;
 import android.app.Application;
 import android.os.StrictMode;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.travazar.tour.packages.data.DataManager;
 
 /**
@@ -26,6 +27,12 @@ public class TravazarApplication extends Application {
                     .build());
         }
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         dataManager = new DataManager();
     }
 
